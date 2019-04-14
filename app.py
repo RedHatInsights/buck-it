f8422663dc9d095f1c9aa0033a1d8ae257f78fc4
Jesse Jaggars <jhjaggars@gmail.com>
2019-04-14 14:21:56 -0500

    adding metrics
    Signed-off-by: Jesse Jaggars <jhjaggars@gmail.com>

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
-import os
-import json
-import aiohttp
-import aiobotocore
-import asyncio
-from aiokafka import KafkaError
+from kafka.errors import KafkaError
 from collections import deque
 from kafkahelpers import make_pair
+import aiobotocore
+import aiohttp
+import asyncio
+import json
+import os
+
+import metrics
 
 loop = asyncio.get_event_loop()
 
-QUEUE = os.environ.get("QUEUE", "platform.upload.buckit")
-RESPONSE_QUEUE = os.environ.get("RESPONSE_QUEUE", "platform.upload.validation")
-BOOT = os.environ.get("KAFKAMQ", "kafka:29092").split(",")
-GROUP = os.environ.get("GROUP", "buckit")
-BUCKET_MAP_FILE = os.environ.get("BUCKET_MAP_FILE")
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_REGION = os.environ.get("AWS_REGION", "us-east-1")
 AWS_SECRET_KEY = os.environ.get("AWS_SECRET_KEY")
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+BOOT = os.environ.get("KAFKAMQ", "kafka:29092").split(",")
+BUCKET_MAP_FILE = os.environ.get("BUCKET_MAP_FILE")
+GROUP = os.environ.get("GROUP", "buckit")
+QUEUE = os.environ.get("QUEUE", "platform.upload.buckit")
+RESPONSE_QUEUE = os.environ.get("RESPONSE_QUEUE", "platform.upload.validation")
 
 try:
     with open(BUCKET_MAP_FILE, "rb") as f:
@@ -44,7 +46,9 @@ async def store(payload, doc):
         aws_access_key_id=AWS_ACCESS_KEY_ID,
     ) as client:
         bucket = BUCKET_MAP[doc["service"]]
-        await client.put_object(Bucket=bucket, Key=doc["payload_id"], Body=payload)
+        with metrics.s3_write_time.time():
+            await client.put_object(Bucket=bucket, Key=doc["payload_id"], Body=payload)
+        metrics.s3_writes.inc()
     session.close()
 
 
@@ -55,7 +59,6 @@ async def consumer(client):
         payload = await fetch(doc)
         await store(payload, doc)
         produce_queue.append({"validation": "handoff"})
-
     await asyncio.sleep(0.5)
 
 
@@ -81,4 +84,5 @@ def make_producer(queue=None):
 if __name__ == "__main__":
     loop.create_task(reader.run(consumer))
     loop.create_task(writer.run(make_producer()))
+    metrics.start()
     loop.run_forever()
---
+++ metrics.py
@@ -0,0 +1,35 @@
+from prometheus_client import Counter, Summary
+from prometheus_client.exposition import choose_encoder, REGISTRY
+from aiohttp import web
+
+
+s3_writes = Counter("buckit_s3_writes", "Total number of s3 writes")
+s3_write_time = Summary("buckit_s3_write_time", "Time spend posting payloads to s3")
+
+
+def _parse_content_type(content_type):
+    if "charset" in content_type:
+        parts = [p.strip() for p in content_type.split(";") if "charset" not in p]
+        return "; ".join(parts)
+    else:
+        return content_type
+
+
+async def metrics_handler(request):
+    registry = REGISTRY
+    encoder, content_type = choose_encoder(request.headers.get("Accept"))
+    print(content_type, _parse_content_type(content_type))
+    if "name[]" in request.query:
+        registry = registry.restricted_registry(request.query["name[]"])
+    try:
+        return web.Response(
+            content_type=_parse_content_type(content_type), body=encoder(registry)
+        )
+    except:
+        raise web.HTTPInternalServerError("error generating metric output")
+
+
+def start():
+    app = web.Application()
+    app.add_routes([web.get("/metrics", metrics_handler)])
+    web.run_app(app)
