bb7b201e4bcf1d92bbf09e5298f57e786b227174
Jesse Jaggars <jjaggars@redhat.com>
2019-04-18 20:04:26 -0500

    bumping to 3.7
    Signed-off-by: Jesse Jaggars <jjaggars@redhat.com>

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from kafkahelpers import make_pair, make_producer
 import aiobotocore
 import aiohttp
 import asyncio
+from contextvars import ContextVar
 import json
 import os
 import logging
@@ -22,6 +23,8 @@ BUCKET_MAP_FILE = os.environ.get("BUCKET_MAP_FILE")
 GROUP = os.environ.get("GROUP", "buckit")
 QUEUE = os.environ.get("QUEUE", "platform.upload.buckit")
 RESPONSE_QUEUE = os.environ.get("RESPONSE_QUEUE", "platform.upload.validation")
+REQUEST_ID = ContextVar("request_id")
+REQUEST_ID.set("-1")
 
 try:
     with open(BUCKET_MAP_FILE, "rb") as f:
@@ -30,7 +33,6 @@ except Exception:
     BUCKET_MAP = {}
 
 
-reader, writer = make_pair(QUEUE, GROUP, BOOT)
 produce_queue = deque()
 
 
@@ -40,24 +42,20 @@ async def fetch(doc):
             return await response.read()
 
 
-async def store(payload, doc):
+async def store(payload, bucket):
     session = aiobotocore.get_session(loop=loop)
-    try:
-        async with session.create_client(
-            "s3",
-            region_name=AWS_REGION,
-            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-            aws_access_key_id=AWS_ACCESS_KEY_ID,
-        ) as client:
-            bucket = BUCKET_MAP[doc["category"]]
-            size = len(payload)
-            logger.info("Storing %s len(%s) into bucket '%s'", doc["payload_id"], size, bucket)
-            with metrics.s3_write_time.time():
-                await client.put_object(Bucket=bucket, Key=doc["payload_id"], Body=payload)
-            metrics.payload_size.observe(size)
-            metrics.bucket_counter(bucket).inc()
-    finally:
-        session.close()
+    async with session.create_client(
+        "s3",
+        region_name=AWS_REGION,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+    ) as client:
+        size = len(payload)
+        logger.info("Storing %s bytes into bucket '%s'", size, bucket)
+        with metrics.s3_write_time.time():
+            await client.put_object(Bucket=bucket, Key=REQUEST_ID.get(), Body=payload)
+        metrics.payload_size.observe(size)
+        metrics.bucket_counter.labels(bucket).inc()
 
 
 async def consumer(client):
@@ -65,8 +63,11 @@ async def consumer(client):
     for msgs in (msgs for tp, msgs in data.items()):
         for msg in msgs:
             doc = json.loads(msg.value)
-            payload = await fetch(doc)
-            await store(payload, doc)
+            REQUEST_ID.set(doc["payload_id"])
+            url = doc["url"]
+            payload = await fetch(url)
+            bucket = BUCKET_MAP[doc["category"]]
+            await store(payload, bucket)
             produce_queue.append(
                 {"validation": "handoff", "payload_id": doc["payload_id"]}
             )
@@ -78,6 +79,7 @@ async def handoff(client, item):
 
 
 if __name__ == "__main__":
+    reader, writer = make_pair(QUEUE, GROUP, BOOT)
     loop.create_task(reader.run(consumer))
     c = make_producer(handoff, produce_queue)
     loop.create_task(writer.run(c))
