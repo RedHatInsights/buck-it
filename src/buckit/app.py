9a7e23e0ddb766e6c352b42aadcd00f1f0734dcd
Jesse Jaggars <jjaggars@redhat.com>
2019-07-11 14:21:29 -0500

    payload_id -> request_id
    Signed-off-by: Jesse Jaggars <jjaggars@redhat.com>

--- src/buckit/app.py
+++ src/buckit/app.py
@@ -73,7 +73,7 @@ async def store(payload, bucket):
 def unpack(v, mapping=BUCKET_MAP):
     with metrics.json_loads_time.time():
         doc = json.loads(v)
-    REQUEST_ID.set(doc["payload_id"])
+    REQUEST_ID.set(doc["request_id"])
     return doc["url"], mapping[doc["category"]], doc
 
 
